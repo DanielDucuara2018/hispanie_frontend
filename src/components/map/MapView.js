5a0ff8204dce58f3a370ae2f75e95ecfe69bff0f
DanielDucuara2018 <daniel14015@gmail.com>
2025-03-17 16:03:01 +0100

    Fix events and business routing from map

--- src/components/map/MapView.js
+++ src/components/map/MapView.js
@@ -10,7 +10,7 @@ class MapView extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      selectedEvent: null,
+      selectedItem: null,
       show: false,
     };
   }
@@ -30,7 +30,7 @@ class MapView extends Component {
     });
 
   handleMarkerClick = (event) => {
-    this.setState({ selectedEvent: event, show: true });
+    this.setState({ selectedItem: event, show: true });
   };
 
   handleClose = () => {
@@ -39,8 +39,14 @@ class MapView extends Component {
 
   render() {
     const { events, businesses } = this.props
-    const { selectedEvent } = this.state
+    const { selectedItem } = this.state
 
+    const typeItem = selectedItem !== null ? (selectedItem.id.toLowerCase().includes("event") ? "Event": "Business") : null
+    const routeItem = typeItem != null ? ( typeItem === "Event" ? "/agenda/event" : "/discover/business") : null
+
+    console.log(selectedItem)
+    console.log(typeItem)
+    console.log(routeItem)
     return (
       <div className="map-container">
         <MapContainer center={[47.2184, -1.5536]} zoom={13} style={{ height: "100vh", width: "100%" }}>
@@ -64,30 +70,30 @@ class MapView extends Component {
         {/* Offcanvas (Sidebar Popup) for Event Details */}
         <Offcanvas show={this.state.show} onHide={this.handleClose} placement="start">
           <Offcanvas.Header closeButton>
-            <Offcanvas.Title>Event Details</Offcanvas.Title>
+            <Offcanvas.Title>{typeItem} Details</Offcanvas.Title>
           </Offcanvas.Header>
-          {selectedEvent && (
+          {selectedItem && (
             <Offcanvas.Body>
               <Card>
                   <Card.Img
                     variant="top"
-                    src={selectedEvent.files.find((x) => x.category === "profile_image").path}
+                    src={selectedItem.files.find((x) => x.category === "profile_image").path}
                     className="img-fluid"
                     style={{ height: '200px', objectFit: 'cover' }}
-                    alt={selectedEvent.name} // Always include an alt attribute for accessibility
+                    alt={selectedItem.name} // Always include an alt attribute for accessibility
                   />              
                   <Card.Body>
-                  <Card.Title>{selectedEvent.name}</Card.Title>
-                  <Card.Text style={{ whiteSpace: 'pre-line' }}>{selectedEvent.description}</Card.Text>
+                  <Card.Title>{selectedItem.name}</Card.Title>
+                  <Card.Text style={{ whiteSpace: 'pre-line' }}>{selectedItem.description}</Card.Text>
                   <div className="mb-2">
-                    {selectedEvent.tags.map((tag, index) => (
+                    {selectedItem.tags.map((tag, index) => (
                       <Badge key={index} bg="secondary" className="me-1">
                         {tag.name}
                       </Badge>
                     ))}
                   </div>
                   {/* <p className="fw-bold">💰 Adhesion: {selectedEvent.price}</p> */}
-                  <a href={`/agenda/event/${selectedEvent.id}`} style={{ textDecoration: 'none' }}>
+                  <a href={`${routeItem}/${selectedItem.id}`} style={{ textDecoration: 'none' }}>
                     <Button variant="dark" className="w-100">See More</Button>
                   </a>
                 </Card.Body>
